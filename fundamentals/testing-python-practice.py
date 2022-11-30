4dbd5e3ef33c3a6d786b4e8a622c9261cff39b43
Grant Brown <gbrownzzzz58@gmail.com>
2022-11-30 16:33:05 -0500

    added files

---
+++ fundamentals/testing-python-practice.py
@@ -0,0 +1,57 @@
+import random
+
+print('Welcome to Python!')
+
+print('This is a loop printing 5 times')
+for x in range(1, 6):
+    print(f'x is: {x}')
+
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+day = random.choice(weekdays)
+
+print(f'Today is: {day}')
+
+if day == 'Monday':
+    print('It will be a long week!')
+elif(day == 'Friday'):
+    print('Woohoo, time for the weekend!')
+else:
+    print('Not quite there yet.')
+
+int_to_float = float(35)
+float_to_int = int(44.2)
+int_to_complex = complex(35)
+print(int_to_float)
+print(float_to_int)
+print(int_to_complex)
+print(type(int_to_float))
+print(type(float_to_int))
+print(type(int_to_complex))
+
+import random
+print(random.randint(2,5)) # provides a random number between 2 and 5
+
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print(f"My name is {first_name} {last_name} and I am {age} years old.")
+
+x=1
+y=2
+
+# print(x + str(y))
+print(str(x) + str(y))
+print(int(x) + y)
+print(x+y)
+
+Charles = "Name Charles"
+name = Charles
+print(name)
+
+'hello' + 'world'
+# Hello + World
+# 'Hello' . 'World'
+
+x=1
+y=2
+print(x+y)
